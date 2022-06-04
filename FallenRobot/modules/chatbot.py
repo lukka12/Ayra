f08ca3db552f2e27ab1e61412bfeda7f0f12a83f
Shekhar Upadhyay <105312440+NotReallyCarbon@users.noreply.github.com>
2022-06-04 18:01:00 +0530

    Update chatbot.py

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Agora Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
+                "Ayra Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Agora Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
+                "Ayra Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -126,7 +126,7 @@ def chatbot(update: Update, context: CallbackContext):
 
 def list_all_chats(update: Update, context: CallbackContext):
     chats = sql.get_all_kuki_chats()
-    text = "<b>Agora Enabled Chats</b>\n"
+    text = "<b>Ayra Enabled Chats</b>\n"
     for chat in chats:
         try:
             x = context.bot.get_chat(int(*chat))
